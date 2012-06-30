4afa4161cc46d9d0b8689fcdea9abf111441df2b
Fabien Fleurey <fabien@fleurey.com>
2012-06-30 22:43:21 +0200

    beginning fragments

--- gen/net/modelbased/sensapp/android/sensappdroid/R.java
+++ gen/net/modelbased/sensapp/android/sensappdroid/R.java
@@ -14,10 +14,13 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int flush_database=0x7f06000f;
-        public static final int flush_uploaded=0x7f060010;
+        public static final int flush_database=0x7f060012;
+        public static final int flush_uploaded=0x7f060013;
+        public static final int fragment_measure_details=0x7f06000d;
+        public static final int fragment_measures_list=0x7f06000c;
+        public static final int fragment_view=0x7f06000e;
         public static final int icon=0x7f060000;
-        public static final int insert=0x7f06000c;
+        public static final int insert=0x7f06000f;
         public static final int label=0x7f060001;
         public static final int measure_id=0x7f060004;
         public static final int measure_id_value=0x7f060005;
@@ -29,16 +32,18 @@ public final class R {
         public static final int measure_time_value=0x7f060007;
         public static final int measure_value=0x7f06000a;
         public static final int measure_value_value=0x7f06000b;
-        public static final int stop_service=0x7f06000e;
-        public static final int upload=0x7f06000d;
-        public static final int view=0x7f060011;
+        public static final int stop_service=0x7f060011;
+        public static final int upload=0x7f060010;
+        public static final int view=0x7f060014;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
-        public static final int measure_list=0x7f030001;
-        public static final int measure_row=0x7f030002;
-        public static final int measure_view=0x7f030003;
-        public static final int sensor_row=0x7f030004;
+        public static final int measure_list=0x7f030000;
+        public static final int measure_row=0x7f030001;
+        public static final int measure_view=0x7f030002;
+        public static final int measures=0x7f030003;
+        public static final int sensor_list=0x7f030004;
+        public static final int sensor_row=0x7f030005;
+        public static final int sensors=0x7f030006;
     }
     public static final class menu {
         public static final int list_menu=0x7f050000;
@@ -46,18 +51,19 @@ public final class R {
     public static final class string {
         public static final int app_name=0x7f040001;
         public static final int hello=0x7f040000;
-        public static final int measure_basetime=0x7f040008;
-        public static final int measure_id=0x7f040005;
-        public static final int measure_notuploaded=0x7f04000c;
-        public static final int measure_sensor=0x7f040006;
-        public static final int measure_status=0x7f04000a;
-        public static final int measure_time=0x7f040009;
-        public static final int measure_uploaded=0x7f04000b;
-        public static final int measure_value=0x7f040007;
-        public static final int menu_delete=0x7f040004;
-        public static final int menu_insert=0x7f040003;
+        public static final int measure_basetime=0x7f040009;
+        public static final int measure_id=0x7f040006;
+        public static final int measure_notuploaded=0x7f04000d;
+        public static final int measure_sensor=0x7f040007;
+        public static final int measure_status=0x7f04000b;
+        public static final int measure_time=0x7f04000a;
+        public static final int measure_uploaded=0x7f04000c;
+        public static final int measure_value=0x7f040008;
+        public static final int menu_delete=0x7f040005;
+        public static final int menu_insert=0x7f040004;
         public static final int no_measures=0x7f040002;
-        public static final int toast_service_started=0x7f04000d;
-        public static final int toast_service_stopped=0x7f04000e;
+        public static final int no_sensors=0x7f040003;
+        public static final int toast_service_started=0x7f04000e;
+        public static final int toast_service_stopped=0x7f04000f;
     }
 }
