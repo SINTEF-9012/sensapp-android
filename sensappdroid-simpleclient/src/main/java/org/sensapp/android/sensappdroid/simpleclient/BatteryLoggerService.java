d8cf29d86558fb1528a0fb855834c0ba0d4fed1e
Fabien Fleurey <fabien@fleurey.com>
2012-09-10 16:48:13 +0200

    Use service to log battery

---
+++ sensappdroid-simpleclient/src/main/java/org/sensapp/android/sensappdroid/simpleclient/BatteryLoggerService.java
@@ -0,0 +1,71 @@
+package org.sensapp.android.sensappdroid.simpleclient;
+
+import org.sensapp.android.sensappdroid.contract.SensAppContract;
+
+import android.app.Service;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.BatteryManager;
+import android.os.IBinder;
+import android.util.Log;
+
+public class BatteryLoggerService extends Service {
+
+	private static final String TAG = BatteryLoggerService.class.getSimpleName();
+	private static final String sensorName = "Battery";
+	
+	@Override
+	public void onCreate() {
+		Log.d(TAG, "__ON_CREATE__");
+		super.onCreate();
+		registerSensor();
+		Intent batteryStatus = registerReceiver(null, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+		int level = batteryStatus.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);	
+		Log.d(TAG, "Battery level: " + level);
+		registerMeasure(level);
+		stopSelf();
+	}
+	
+	@Override
+	public void onDestroy() {
+		Log.d(TAG, "__ON_DESTROY__");
+		super.onDestroy();
+	}
+	
+	private void registerSensor() {
+		// Check if the sensor is already registered.
+		Cursor c = getContentResolver().query(Uri.parse(SensAppContract.Sensor.CONTENT_URI + "/" + sensorName), null, null, null, null);
+		boolean isRegistered = c.getCount() > 0;
+		c.close();
+		if (isRegistered) {
+			// The sensor is already registered.
+			Log.w(TAG, sensorName + " is already registered");
+		} else {
+			// The sensor is not known, register it.
+			ContentValues values = new ContentValues();
+			values.put(SensAppContract.Sensor.NAME, sensorName);
+			values.put(SensAppContract.Sensor.UNIT, "%");
+			values.put(SensAppContract.Sensor.BACKEND, "raw");
+			values.put(SensAppContract.Sensor.TEMPLATE, "numerical");
+			values.put(SensAppContract.Sensor.UPLOADED, 0);
+			getContentResolver().insert(SensAppContract.Sensor.CONTENT_URI, values);
+		}
+	}
+	
+	private void registerMeasure(int value) {
+		ContentValues values = new ContentValues();
+		values.put(SensAppContract.Measure.SENSOR, sensorName);
+		values.put(SensAppContract.Measure.VALUE, String.valueOf(value));
+		values.put(SensAppContract.Measure.BASETIME, 0);
+		values.put(SensAppContract.Measure.TIME, System.currentTimeMillis());
+		values.put(SensAppContract.Measure.UPLOADED, 0);
+	}
+	
+	@Override
+	public IBinder onBind(Intent arg0) {
+		return null;
+	}
+}
--- sensappdroid-simpleclient/src/main/java/org/sensapp/android/sensappdroid/simpleclient/MainActivity.java
+++ sensappdroid-simpleclient/src/main/java/org/sensapp/android/sensappdroid/simpleclient/MainActivity.java
@@ -1,20 +1,54 @@
 package org.sensapp.android.sensappdroid.simpleclient;
 
 import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
+	private boolean serviceRunning = true;
+	private Button buttonService;
+	private TextView tvStatus;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        serviceRunning = (PendingIntent.getService(getApplicationContext(), 0, new Intent(getApplicationContext(), BatteryLoggerService.class), PendingIntent.FLAG_NO_CREATE) != null);
+        buttonService = (Button) findViewById(R.id.b_status);
+        tvStatus = (TextView) findViewById(R.id.tv_status);
+        updateLabels();
+        buttonService.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				AlarmManager alarm = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+				Intent intent = new Intent(MainActivity.this.getApplicationContext(), BatteryLoggerService.class);
+				PendingIntent pintent = PendingIntent.getService(MainActivity.this.getApplicationContext(), 0, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+				if (!serviceRunning) {
+					serviceRunning = true;
+					alarm.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), 10*1000, pintent);
+				} else {
+					serviceRunning = false;
+					alarm.cancel(pintent);
+				}
+				updateLabels();
+			}
+		});
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
+    
+    private void updateLabels() {
+    	if (serviceRunning) {
+        	buttonService.setText(R.string.button_service_stop);
+        	tvStatus.setText(R.string.tv_status_running);
+        } else {
+        	buttonService.setText(R.string.button_service_start);
+        	tvStatus.setText(R.string.tv_status_stoped);
+        }
     }
 }
