dc8ea54bfaaa16936421ad4e92b841dddc05859a
Fabien Fleurey <fabien@fleurey.com>
2012-09-13 16:22:52 +0200

    Set the period to 60 seconds

--- sensappdroid-simpleclient/src/main/java/org/sensapp/android/sensappdroid/clientsamples/batterylogger/AlarmHelper.java
+++ sensappdroid-simpleclient/src/main/java/org/sensapp/android/sensappdroid/clientsamples/batterylogger/AlarmHelper.java
@@ -9,7 +9,7 @@ import android.content.Intent;
 
 public class AlarmHelper {
 
-	protected static final int REFRESH_RATE = 10; // Polling interval in seconds to start the service.
+	protected static final int REFRESH_RATE = 60; // Polling interval in seconds to start the service.
 	
 	private static final int ACTIVE_NOTIFICATION_ID = 79290;
 	
@@ -18,7 +18,7 @@ public class AlarmHelper {
 		PendingIntent pendingIntent = PendingIntent.getService(context, 0, startService, PendingIntent.FLAG_CANCEL_CURRENT);
 		((AlarmManager) context.getSystemService(Context.ALARM_SERVICE)).setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), REFRESH_RATE * 1000, pendingIntent);
 		
-		@SuppressWarnings("deprecation")
+		@SuppressWarnings("deprecation") // We don't want target only API 16...
 		Notification notification = new Notification.Builder(context)
 			.setContentTitle("BatLog active")
 			.setContentText("BatLog active")
