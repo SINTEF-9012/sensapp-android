a403a40b59fd502941a53a4a6bb1777bac2e95f9
Fabien Fleurey <fabien@fleurey.com>
2012-09-16 13:12:07 +0200

    use queue for better performance

--- sensappdroid-contract/src/main/java/org/sensapp/android/sensappdroid/api/InsertionManager.java
+++ sensappdroid-contract/src/main/java/org/sensapp/android/sensappdroid/api/InsertionManager.java
@@ -1,6 +1,6 @@
 package org.sensapp.android.sensappdroid.api;
 
-import java.util.ArrayList;
+import java.util.ArrayDeque;
 import java.util.Hashtable;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
@@ -14,35 +14,37 @@ import android.util.Log;
 
 public class InsertionManager {
 
-	private final static int LOWEST_INTERVAL = 5000;
+	private final static int LOWEST_INTERVAL = 1;
 	
-	private static Hashtable<String, ArrayList<ContentValues>> buffers = new Hashtable<String, ArrayList<ContentValues>>();
+	private static Hashtable<String, ArrayDeque<ContentValues>> buffers = new Hashtable<String, ArrayDeque<ContentValues>>();
 	
 	private static final ScheduledExecutorService worker = Executors.newSingleThreadScheduledExecutor();
 	
 	public static void storeMeasure(final Context context, ContentValues values) {
+		//Log.w("DEBUG", "__Store__");
 		final String sensor = (String) values.get(SensAppContract.Measure.SENSOR);
-		Log.w("DEBUG", "__Store__");
-		synchronized(buffers) {
-			if (!buffers.containsKey(sensor)) { 
-				buffers.put(sensor, new ArrayList<ContentValues>());
-				worker.schedule(new Runnable() {
-					public void run() {
-						flushSensorMeasures(context, sensor);
-					}
-				}, LOWEST_INTERVAL, TimeUnit.MILLISECONDS);
-			}
-			buffers.get(sensor).add(values);
+		if (!buffers.containsKey(sensor)) { 
+			buffers.put(sensor, new ArrayDeque<ContentValues>());
+			scheduleFlush(context, sensor);
 		}
+		buffers.get(sensor).add(values);
+	}
+
+	private static void flushSensorMeasures(Context context, String name) {
+		Log.v("DEBUG", "__Flush__");
+		ContentValues values = buffers.get(name).pollFirst(); 
+		while (values != null) {
+			context.getContentResolver().insert(SensAppContract.Measure.CONTENT_URI, values);
+			values = buffers.get(name).pollFirst();
+		}
+		scheduleFlush(context, name);
 	}
 	
-	private static void flushSensorMeasures(Context context, String name) {
-		synchronized(buffers) {
-			for (ContentValues values : buffers.get(name)) {
-				context.getContentResolver().insert(SensAppContract.Measure.CONTENT_URI, values);
+	private static void scheduleFlush(final Context context, final String sensor) {
+		worker.schedule(new Runnable() {
+			public void run() {
+				flushSensorMeasures(context, sensor);
 			}
-		Log.e("DEBUG", "__Flush__");
-		buffers.remove(name);
-		}
+		}, LOWEST_INTERVAL, TimeUnit.SECONDS);
 	}
 }
