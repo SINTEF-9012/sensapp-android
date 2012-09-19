a679c2aa0487a381dffbb570289d593def6f7ee3
Fabien Fleurey <fabien@fleurey.com>
2012-09-19 23:54:58 +0200

    Add headers

--- sensappdroid-test/gen/org/sensapp/android/sensappdroid/test/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package org.sensapp.android.sensappdroid.test;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
