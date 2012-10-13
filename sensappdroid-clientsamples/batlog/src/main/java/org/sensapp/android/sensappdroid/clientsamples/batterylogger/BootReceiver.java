5e83ad443fe47f51904f66c596082a4a21e0f9e6
Fabien Fleurey <fabien@fleurey.org>
2012-10-14 01:25:24 +0200

    Remove debug

--- sensappdroid-clientsamples/batlog/src/main/java/org/sensapp/android/sensappdroid/clientsamples/batterylogger/BootReceiver.java
+++ sensappdroid-clientsamples/batlog/src/main/java/org/sensapp/android/sensappdroid/clientsamples/batterylogger/BootReceiver.java
@@ -19,16 +19,13 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.preference.PreferenceManager;
-import android.util.Log;
 
 public class BootReceiver extends BroadcastReceiver {
 
 	@Override
 	public void onReceive(Context context, Intent intent) {
-		Log.e("DEBUG", "Boot completed");
 		if (PreferenceManager.getDefaultSharedPreferences(context).getBoolean(MainActivity.SERVICE_RUNNING, false)) {
 			AlarmHelper.setAlarm(context);
 		}
 	}
-
 }
